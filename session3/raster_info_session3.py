fea661179a2090125fca63c8dff77ae424d82d7e
Rich Sharp <richpsharp@gmail.com>
2020-09-24 19:12:20 -0700

    rename raster info so it's different in session 3

---
+++ session3/raster_info_session3.py
@@ -0,0 +1,7 @@
+"""Demo some raster info."""
+import pygeoprocessing
+
+if __name__ == '__main__':
+    raster_path = '../session2/LULC_2000_md5_e0523f6f5c4bc8e9ee4865958fe876e0.tif'
+    raster_info = pygeoprocessing.get_raster_info(raster_path)
+    print(raster_info)
